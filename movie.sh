2448815dea3e0b5ab14e16e284635c47646073b4
gronki <gronki@gmail.com>
2016-03-04 15:18:39 +0100

    fps=24

--- movie.sh
+++ movie.sh
@@ -10,6 +10,6 @@ mkdir -p mpeg
 LISTF=$(ls png/* | grep "png\/${PREFX}-1[0-9]*\.png")
 
 
-ffmpeg -i png/${PREFX}-1%06d.png -r 20 -b:v 5M -s 1600x900 -vcodec h264   mpeg/${PREFX}.mpeg
+ffmpeg -i png/${PREFX}-1%06d.png -r 24 -b:v 8M -s 1600x900 -vcodec h264   mpeg/${PREFX}.mpeg
 
 rm $LISTF
